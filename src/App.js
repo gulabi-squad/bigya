d968e51f609c470fbdd757baa21ecb318ac94172
professor-z <sudipgautam50@gmail.com>
2023-03-10 14:58:17 +0545

    first push homepage helped by pujan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+import Homepage from './components/Homepage';
 
-function App() {
-  return (
+function App(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <Router>
+        <Routes>
+        <Route path='dashboard/' element={<Homepage/>}></Route>
+
+        </Routes>
+
+      </Router>
+
+    </div>)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// working end
+
+
 
 export default App;
