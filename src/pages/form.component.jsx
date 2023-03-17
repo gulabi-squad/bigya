da122822763f10cefa58e016a9bb35485428f769
sa9arr <sagarkandel2058@gmail.com>
2023-03-18 00:03:26 +0545

    forms component

--- src/pages/form.component.jsx
+++ src/pages/form.component.jsx
@@ -4,9 +4,13 @@ const Clientform = () => {
     return (
         <div>
             <div className="flex justify-center">
-                <div className=" flex justify-center w-[50rem] h-[32rem] border-2 mt-20" > 
+                <div className=" flex flex-col gap-4 justify-center items-center w-[50rem] h-[32rem] border-2 mt-20 text-opacity-70" > 
 
-                <div> Name: <input className="width-[417px]  border-b-2 border-solid border-black"/>  </div> 
+                <div className="text-opacity-50"> Name: <input className="width-[417px]  border-b-2 border-solid border-black"/>  </div> 
+                <div> Date: <input className="width-[417px]  border-b-2 border-solid border-black"/>  </div> 
+                <div> Time: <input className="width-[417px]  border-b-2 border-solid border-black"/>  </div> 
+                <div> Location: <input className="width-[417px]  border-b-2 border-solid border-black"/>  </div> 
+                <div> Description: <input className="width-[30rem] h-[10rem] border-2 border-solid border-black"/>  </div> 
                 </div>
 
             </div>
