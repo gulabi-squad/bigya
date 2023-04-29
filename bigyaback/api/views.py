27b2471de0f58a11a709e0620c30551a5fa0a23f
ShishirSap <shishir.sap83@gmail.com>
2023-04-29 18:24:24 +0545

    status update

--- bigyaback/api/views.py
+++ bigyaback/api/views.py
@@ -78,9 +78,9 @@ class UserLoginView(APIView):
     user = authenticate(email=email, password=password)
     if user is not None:
       token = get_tokens_for_user(user)
-      return Response({'token':token, 'msg':'Login Success'}, status=status.HTTP_200_OK)
+      return Response({'token':token, 'msg':'Login Success', 'status':200})
     else:
-      return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
+      return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}, 'status':400})
 
 
 
