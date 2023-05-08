1f3b1d6ba2e1312a73772a191e6b5c5f8bc78947
sa9arr <sagarkandel2058@gmail.com>
2023-05-08 13:30:44 +0545

    hire form embedded in details page

--- src/pages/Expertdetails.js
+++ src/pages/Expertdetails.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 const Expertdetails = () => {
   return ( 
 <div>
- <div className="w-[230px] h-[197.2px] ml-[125px] mt-[140px] absolute"> This is details page <img src={doctorimage} />
+ <div className="w-[230px] h-[197.2px] ml-[125px] mt-[100px] absolute">  <img src={doctorimage} />
   <div className="text-center mt-3 font-bold"> Puja Lamichhane </div>
   <div className="text-center font-light text-opacity-60"> Psychiatrist </div>
  <div className="ml-14 p-4 flex items-center text-sm text-gray-600"><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 fill-current text-gray-400"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><span className="ml-2"></span></div>
@@ -14,16 +14,43 @@ const Expertdetails = () => {
  </div>
  <div className=" absolute w-[409px] h-[51px] ml-[63px] mt-[446px]  max-w-full">
  I am Puja Lamichhane. I am doctor gratuated from the university of XYZ.I have 5 yrs of experince working as a doctor. 
-    <div className="flex justify-center text-center">
-    <div className="bg-green-800 mt-6 w-[150px] h-[50px] border rounded-xl text-white font-bold py-2 text-[20px]"> <Link to="/clientform"> HIRE ME</Link> </div>
-    </div>
+    
 </div>
 
+<div className="ml-[25rem]">
+            <div className="flex justify-center">
+                <div className=" flex flex-col gap-4 justify-center items-center w-[50rem] h-[32rem] border-2 mt-12 text-opacity-70" > 
+
+                <div className="text-opacity-50"> Name: <input className="ml-4 width-[417px]  border-b-2 border-solid border-indigo-400"/>  </div> 
+                <div> Date: <input  type="date" className="ml-4 w-[16rem]  border-b-2 border-solid border-indigo-400"/>  </div> 
+                <div> Time: <input type="time" className="w-[16rem] ml-4 width-[417px]  border-b-2 border-solid border-indigo-400"/>  </div> 
+                <div> Location: <input className="ml-4 width-[417px]  border-b-2 border-solid border-indigo-400"/>  </div> 
+                <div className="ml-[9rem]"> Description: <input className="w-[26rem] h-[10rem] border-2 border-solid border-indigo-400"/>  </div> 
+                <div className="flex justify-center text-center">
+    <div className="ml-[7rem] bg-green-800 mt-6 w-[150px] h-[50px] border rounded-xl text-white font-bold py-2 text-[20px]"> <Link to="/clientform"> HIRE ME</Link> </div>
+    </div>
+                </div>
+
+            </div>
 
 
 
 
 
+
+
+        </div>
+
+
+
+       
+
+
+
+
+
+
+        
 
 
 
