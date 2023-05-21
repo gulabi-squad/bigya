4d39245861ff93cf3261a391cc42c800a745250c
sa9arr <sagarkandel2058@gmail.com>
2023-05-22 00:10:30 +0545

    forums ui decorated

--- src/pages/Forums.jsx
+++ src/pages/Forums.jsx
@@ -2,25 +2,24 @@ import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import AuthContext from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import  userimage  from './user.jpeg'
 
 const BASE_URL = 'http://localhost:8000';
 
 function Allforums() {
-  const {tokens}=useContext(AuthContext)
+  const { tokens } = useContext(AuthContext);
   const [posts, setPosts] = useState([]);
   const [newPost, setNewPost] = useState({ title: '', contentvalue: '' });
   const [newComment, setNewComment] = useState({ postid: '', comment: '' });
-  const [searchkey,setSearchkey]=useState('')
-  let navigate=useNavigate()
-  const handlesubmit=(e)=>{
-    console.log('search')
-    
-    console.log(searchkey)
-    navigate(`/forums/search/${searchkey}`)
+  const [searchKey, setSearchKey] = useState('');
+  const navigate = useNavigate();
 
-
-  }
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('search');
+    console.log(searchKey);
+    navigate(`/forums/search/${searchKey}`);
+  };
 
   useEffect(() => {
     fetchPosts();
@@ -29,7 +28,7 @@ function Allforums() {
   const fetchPosts = async () => {
     try {
       const response = await axios.get(`${BASE_URL}/forums/`);
-      console.log(response.data)
+      console.log(response.data);
       setPosts(response.data.data);
     } catch (error) {
       console.error(error);
@@ -38,10 +37,10 @@ function Allforums() {
 
   const createPost = async () => {
     try {
-      await axios.post(`${BASE_URL}/forums/`, newPost,{
+      await axios.post(`${BASE_URL}/forums/`, newPost, {
         headers: {
-          Authorization: `Bearer ${tokens.access}`
-      }
+          Authorization: `Bearer ${tokens.access}`,
+        },
       });
       fetchPosts();
       setNewPost({ title: '', contentvalue: '' });
@@ -52,18 +51,11 @@ function Allforums() {
 
   const createComment = async () => {
     try {
-      await axios.post(`${BASE_URL}/comments/`, newComment,{
+      await axios.post(`${BASE_URL}/comments/`, newComment, {
         headers: {
-          Authorization: `Bearer ${tokens.access}`
-      }
-      })
-      .then(response => {
-        console.log(response)
-      })
-      .catch(error => {
-        console.log(error);
+          Authorization: `Bearer ${tokens.access}`,
+        },
       });
-
       fetchPosts();
       setNewComment({ postid: '', comment: '' });
     } catch (error) {
@@ -72,69 +64,82 @@ function Allforums() {
   };
 
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold mb-4">Forum</h1>
-      <form onSubmit={handlesubmit}>
-            <input
-              type="text"
-              name="search"
-              onChange={(e)=>setSearchkey(e.target.value)}
-              placeholder="Search"
-              className="bg-white-200 text-gray-800 px-4 py-2 rounded-xl border border-black-200 focus:outline-none focus:ring focus:ring-blue-300"
-            />
-            <input type="submit" className="text-red-700" value="Search"/>
-
-</form>
+    <div className="container mx-auto ">
+      <h1 className="text-3xl font-bold font-mono">Bigya-Forums</h1>
+      <p className="mb-4 font-roboto italic opacity-50"> A place to get all your queries answered </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          onChange={(e) => setSearchKey(e.target.value)}
+          placeholder="Search"
+          className="bg-white-200 text-gray-800 px-4 py-2 rounded-xl border border-black-200 focus:outline-none focus:ring focus:ring-blue-300"
+        />
+        <input type="submit" className=" ml-4 w-16 mb-4 h-8 bg-blue-500 rounded text-white font-bold  " value="Search" />
+      </form>
       <div className="mb-8">
         <h2 className="text-xl font-semibold mb-2">Create a Post</h2>
-        <input
-          className="border border-gray-300 rounded p-2 mr-2"
-          type="text"
-          placeholder="Title"
-          value={newPost.title}
-          onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
-        />
-        <textarea
-          className="border border-gray-300 rounded p-2 mr-2"
-          placeholder="Content"
-          value={newPost.contentvalue}
-          onChange={(e) => setNewPost({ ...newPost, contentvalue: e.target.value })}
-        />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={createPost}
-        >
-          Create Post
-        </button>
+        <div className="flex flex-col space-y-2">
+          <input
+            className="border border-gray-300 rounded p-2"
+            type="text"
+            placeholder="Title"
+            value={newPost.title}
+            onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
+          />
+          <textarea
+            className="border border-gray-300 rounded p-2"
+            placeholder="Content"
+            value={newPost.contentvalue}
+            onChange={(e) =>
+              setNewPost({ ...newPost, contentvalue: e.target.value })
+            }
+          />
+          <div className='flex justify-center'>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-[10rem] flex text-center justify-center "
+            onClick={createPost}
+          >
+            Create Post
+          </button>
+          </div>
+        </div>
       </div>
 
       {posts.map((post) => (
         <div key={post.id} className="border border-gray-300 rounded p-4 mb-4">
           <h2 className="text-xl font-semibold">{post.title}</h2>
           <p>{post.contentvalue}</p>
-         <div className="mt-4">
+          <div className="mt-4">
             <h3 className="text-lg font-semibold mb-2">Comments</h3>
             {post.comments.map((comment) => (
-              <p className="mb-2">
-                {comment.comment}
-              </p>
+              <div className="flex items-center space-x-2 mb-2">
+                <img
+                  className="w-10 h-6 rounded-full"
+                  src={userimage}
+                  alt="Avatar"
+                />
+                <p>{comment.comment}</p>
+              </div>
             ))}
 
-            <input
-              className="border border-gray-300 rounded p-2 mr-2"
-              type="text"
-              placeholder="Add a comment"
-              value={newComment.content}
-              onChange={(e) =>
-                setNewComment({ postid: post.id, comment: e.target.value })
-              }
-            />
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={createComment}
-            >
-              Add Comment
-            </button>
+            <div className="flex space-x-2">
+              <input
+                className="border border-gray-300 rounded p-2 flex-1"
+                type="text"
+                placeholder="Add a comment"
+                value={newComment.content}
+                onChange={(e) =>
+                  setNewComment({ postid: post.id, comment: e.target.value })
+                }
+              />
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={createComment}
+              >
+                Add Comment
+              </button>
+            </div>
           </div>
         </div>
       ))}
