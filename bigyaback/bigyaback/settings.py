b18bd1aaa0aae5e0bcb3ed5a38ec9718434f3a37
ShishirSap <shishir.sap83@gmail.com>
2023-05-22 00:58:01 +0545

    mysql configurations done

--- bigyaback/bigyaback/settings.py
+++ bigyaback/bigyaback/settings.py
@@ -94,10 +94,17 @@ WSGI_APPLICATION = 'bigyaback.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'newbigya',  
+        # 'USER': 'notesadmin',  
+        # 'PASSWORD': '',  
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+       
+    }  
 }
 
 
